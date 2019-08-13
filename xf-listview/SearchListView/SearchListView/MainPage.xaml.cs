e599cbd3eb6f379b1a8c29c54dab5eb6c5305d83
851265601 <851265601@qq.com>
2019-08-13 19:03:30 +0800

    Add files via upload

---
+++ xf-listview/SearchListView/SearchListView/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms;
+
+namespace SearchListView
+{
+    public partial class MainPage : ContentPage
+    {
+        MainPageViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new MainPageViewModel();
+            this.BindingContext = this.viewModel;
+
+        }
+
+        private async void _ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var model = e.Item as ItemObj;
+            await Navigation.PushAsync(new Page1(model));
+        }
+    }
+}
